668a4fe9f3c698568735a8c98f51084e7e3bd8f4
Kush <kushalsaha789@gmail.com>
2025-11-05 21:04:22 +0530

    Added TT

--- Week 5/src/TimeTable.java
+++ Week 5/src/TimeTable.java
@@ -1,8 +1,25 @@
 import java.util.Scanner;
 
-public class TimeTable {
-    public static void main(String[] args) {
+class TimeTable {
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size: ");
+        int n = sc.nextInt();
 
+        System.out.print("   * |");
+        for (int i = 1; i <= n; i++) {
+            System.out.printf("%4d", i);
+        }
+
+        System.out.println();
+        System.out.println("-----" + "-".repeat(n * 4));
+
+        for (int i = 1; i <= n; i++) {
+            System.out.printf("%4d |", i);
+            for (int j = 1; j <= n; j++) {
+                System.out.printf("%4d", i * j);
+            }
+            System.out.println();
+        }
     }
 }
